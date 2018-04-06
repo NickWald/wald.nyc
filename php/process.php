18f668e14cb1b4c7b1f9cc53c6e36d457ea5653e
Nick Wald <info@nickwald.tech>
2018-04-05 21:06:09 -0400

    (FINALLY) some changes to contact so now it hopefully works

---
+++ php/process.php
@@ -0,0 +1,14 @@
+<?php
+    $to = "nicholas.wald@gmail.com";
+    $from = $_REQUEST['email'];
+    $name = $_REQUEST['name'];
+    $headers = "From: $from";
+    $subject = "NEW MESSAGE FROM WALD.NYC";
+    $fields = array();
+    $fields{"name"} = "name";
+    $fields{"email"} = "email";
+    $fields{"subject"} = "subject";
+    $fields{"message"} = "message";
+    $body = "Here is what was sent:\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
+    $send = mail($to, $subject, $body, $headers);
+?>
